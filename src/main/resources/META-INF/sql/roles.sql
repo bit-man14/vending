5bdb5687f2b4b607d9e6c1a09a0ac8fb558e6eee
bit-man14 <bt.kalwasinscy@gmail.com>
2020-11-21 11:09:39 +0100

    Products OK

--- src/main/resources/META-INF/sql/roles.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO roles (name) VALUES ('ADMIN');
-INSERT INTO roles (name) VALUES ('USER');
-INSERT INTO roles (name) VALUES ('READER');
-
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
-INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
-INSERT INTO users_roles (user_id, role_id) VALUES (2, 3);
-INSERT INTO users_roles (user_id, role_id) VALUES (3, 2);
-INSERT INTO users_roles (user_id, role_id) VALUES (4, 3);
