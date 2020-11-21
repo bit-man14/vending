325f4359823999196e1b5163989b9744d2606337
bit-man14 <bt.kalwasinscy@gmail.com>
2020-11-21 11:27:31 +0100

    autoincrement set to 1

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,3 +1,4 @@
+SET @@auto_increment_increment=1;
 INSERT INTO users (email, name, password, active) VALUES ('tom@gmail.com','Tom', '$2a$10$BG4Khz7k/8pS0JyU4WI0CeiDDGeXREmffzAA9xwU91o5Clu9QKNPe', TRUE);
 INSERT INTO users (email, name, password, active) VALUES ('tom1@gmail.com','Adam', '$2a$10$BG4Khz7k/8pS0JyU4WI0CeiDDGeXREmffzAA9xwU91o5Clu9QKNPe', TRUE);
 INSERT INTO users (email, name, password, active) VALUES ('tom2@gmail.com','Guest', '$2a$10$BG4Khz7k/8pS0JyU4WI0CeiDDGeXREmffzAA9xwU91o5Clu9QKNPe', TRUE);
